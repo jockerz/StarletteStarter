66ffd79c722d226e543e81570ffcd044c1e3ceeb
user <user@localhost.localdomain>
2024-12-27 11:57:18 +0700

    initial commit for adminlte

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,58 @@
-// Theme switch
-!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";var e,t="tablerTheme",n=new Proxy(new URLSearchParams(window.location.search),{get:function(e,t){return e.get(t)}});if(n.theme)localStorage.setItem(t,n.theme),e=n.theme;else{var o=localStorage.getItem(t);e=o||"light"}document.body.classList.remove("theme-dark","theme-light"),document.body.classList.add("theme-".concat(e))}));
+// Language
 function setLang(language) {
-    document.cookie = `language=${language}; samesite=lax; path=/`;
+  document.cookie = `language=${language}; samesite=lax; path=/`;
+  for (const lang of ['en', 'id']) {
+    elem = document.getElementById(`lang-${lang}`)
+    lang === language ? elem.classList.add('active') : elem.classList.remove('active')
+  }
+}
+
+// Theme
+function setTheme(theme) {
+  el_nav = document.getElementById('top-navbar')
+  if (el_nav !== null) {
+    el_nav.classList.remove('navbar-light')
+    el_nav.classList.remove('navbar-dark')
+  }
+
+  theme_switch = document.getElementById('theme-switch')
+
+  if (theme_switch !== null) {
+    theme_switch.innerHTML = '';
+  }
+
+  if (theme === 'dark') {
+    document.body.classList.add('dark-mode')
+    if (el_nav !== null) {
+      el_nav.classList.add('navbar-dark')
+    }
+    if (theme_switch !== null) {
+      theme_switch.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+  } else {
+    document.body.classList.remove('dark-mode')
+    if (el_nav !== null) {
+      el_nav.classList.add('navbar-light')
+    }
+    if (theme_switch !== null) {
+      theme_switch.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+  }
+
+  localStorage.setItem('theme', theme)
+}
+
+function initTheme() {
+  const theme = localStorage.getItem('theme');
+  if (theme !== undefined && theme !== null) {
+    setTheme(theme)
+  } else {
+     const checkIsDarkSchemePreferred = () => window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches ?? false;
+     setTheme(checkIsDarkSchemePreferred ? 'dark' : 'light')
+  }
+}
+
+function themeSwitch() {
+  theme = localStorage.getItem('theme') || 'light';
+  setTheme(theme === 'light' ? 'dark': 'light')
 }
